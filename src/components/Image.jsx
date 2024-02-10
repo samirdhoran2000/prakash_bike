1cf4acb92ba325d820e8d61ab68496af8dc593a2
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-02-10 17:35:28 +0530

    slogan add to hero image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,21 @@
 import './image.css'
 const Image = () => {
   return (
-      <div className='image-main-container' id='home'>
-        <img className='image-tag' src="/adventure1.jpg" alt="img" />  
-        
+    <div
+      className="image-main-container"
+      id="home"
+      
+    >
+      <div>
+
+
+      <h1>
+        Start Your Journey Confidently with Us
+      </h1>
+      </div>
+      {/* <img className="image-tag" src="/adventure1.jpg" alt="img" /> */}
     </div>
-  )
+  );
 }
 
 export default Image
