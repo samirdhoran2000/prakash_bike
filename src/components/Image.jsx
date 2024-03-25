4380ac4ab95fb7fde8f67bc26c6268290623bd1f
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-03-25 16:26:12 +0530

    google ads and something change

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,21 +1,13 @@
-import './image.css'
+import "./image.css";
 const Image = () => {
   return (
-    <div
-      className="image-main-container"
-      id="home"
-      
-    >
+    <div className="image-main-container" id="home">
       <div>
-
-
-      <h1>
-        Start Your Journey Confidently with Us
-      </h1>
+        <h1>Start Your Journey Confidently with Us</h1>
       </div>
       {/* <img className="image-tag" src="/adventure1.jpg" alt="img" /> */}
     </div>
   );
-}
+};
 
-export default Image
+export default Image;
