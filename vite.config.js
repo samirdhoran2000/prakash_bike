a1b6e8391f81cf8923a3d08604add80ae7343dc3
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-06-12 13:48:12 +0530

    not more change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     port: 3000,
-    host:true
+    host: true,
+    hmr: {
+      overlay:false
+    }
   },
   plugins: [react()],
 })
